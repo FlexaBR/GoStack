cb3cb76652d987e14ea29383017d7c2074a78b5e
FlexaBR <r.gutheil@terra.com.br>
2020-01-28 15:44:32 -0300

    Final Ambiente e Conceitos

---
+++ F2-M2-Iniciando-Backend/src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import routes from './routes';
+
+class App {
+  constructor() {
+    // Executa automatico (constroi) quando a classe é chamada.
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
